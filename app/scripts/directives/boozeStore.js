65da271dfe1710cd51e11ef7e0b279fa71a8a473
Liam Ronan <liampronan@gmail.com>
2014-06-10 06:13:17 -0700

    some more style changes

---
+++ app/scripts/directives/boozeStore.js
@@ -0,0 +1,20 @@
+
+angular.module('SlushFunApp')
+  .directive('boozeStore', function($compile) {
+
+    var ratingLabelClass = function(searchResultRating){
+      return {'label-success' : searchResultRating > 80,
+              'label-warn' : searchResultRating > 60,
+              'label-danger' : searchResultRating < 60}
+    };
+
+    return {
+      scope: {
+        searchResult: '='
+      },
+      restrict: 'A',
+      replace: 'true',
+      templateUrl: '/views/deliveries.storeSearchResult.html'
+
+    };
+  });
