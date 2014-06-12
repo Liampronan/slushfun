b42cce53f69f9ccd02a19b81027406ffd4820c21
Liam Ronan <liampronan@gmail.com>
2014-06-11 21:00:02 -0700

    hashkey update

--- app/scripts/controllers/groupCartController.js
+++ app/scripts/controllers/groupCartController.js
@@ -86,7 +86,7 @@ angular.module('SlushFunApp')
 
       $scope.removeFromCart = function(cartItemHashKey){
         angular.forEach($scope.cart.items, function(item){
-          console.log(item, cartItemHashKey);
+          console.log(item);
           if (item.$$hashKey === cartItemHashKey){
             $scope.cart.items.splice(cartItemHashKey,1);
             $scope.updateCart();
